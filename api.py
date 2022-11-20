226a30b69491b76efef509975e4cb6092157561d
Guillaume Massol <guillaume.massol@gmail.com>
2022-11-20 20:41:46 +0100

    Reformat some code

--- api.py
+++ api.py
@@ -13,24 +13,6 @@ CORS(app)
 api = Api(app)
 
 
-# Set the paths
-photos_path = Path("featurescoop_salvatore") / "imgs"
-features_path = Path("featurescoop_salvatore") / "features"
-
-# List all JPGs in the folder
-photos_files = list(photos_path.glob("*.jpg"))
-photos_files.sort()
-
-# Load the features and the corresponding IDs
-photo_features = np.load(features_path / "features.npy")
-photo_ids = pd.read_csv(features_path / "photo_ids.csv")
-photo_ids = list(photo_ids['photo_id'])
-
-# Load the open CLIP model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load("checkpoints/ViT-B-32.pt", device=device)
-
-
 def encode_text(txt):
     with torch.no_grad():
 
@@ -53,14 +35,14 @@ class Index(Resource):
 class EncodeText(Resource):
     def get(self):
 
-        txt = request.args.get('text')
+        txt = request.args.get("text")
 
         if txt:
             text_features = encode_text(txt)
             results = text_features[0].tolist()
 
         else:
-            results = 'Missing arguments'
+            results = "Missing arguments"
 
         return jsonify(results)
 
@@ -68,7 +50,7 @@ class EncodeText(Resource):
 class GetClosest(Resource):
     def get(self):
 
-        txt = request.args.get('text')
+        txt = request.args.get("text")
 
         if txt:
             text_features = encode_text(txt)
@@ -77,22 +59,44 @@ class GetClosest(Resource):
             similarities = (text_features @ photo_features.T).squeeze(0).tolist()
 
             # Sort the photos by their similarity score
-            best_photos = sorted(zip(similarities, range(photo_features.shape[0])), key=lambda x: x[0], reverse=True)
+            best_photos = sorted(
+                zip(similarities, range(photo_features.shape[0])),
+                key=lambda x: x[0],
+                reverse=True,
+            )
             # for p in best_photos[:10]:
             #     print(type(p), [str(p[0]), str(p[1])])
             results = best_photos[:10]
 
         else:
-            results = 'Missing arguments'
+            results = "Missing arguments"
 
         return jsonify(results)
 
 
-api.add_resource(Index, '/index')
-api.add_resource(EncodeText, '/encode_text')
-api.add_resource(GetClosest, '/get_closest')
+api.add_resource(Index, "/index")
+api.add_resource(EncodeText, "/encode_text")
+api.add_resource(GetClosest, "/get_closest")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    # Set the paths
+    photos_path = Path("featurescoop_salvatore") / "imgs"
+    features_path = Path("featurescoop_salvatore") / "features"
+
+    # List all JPGs in the folder
+    photos_files = list(photos_path.glob("*.jpg"))
+    photos_files.sort()
+
+    # Load the features and the corresponding IDs
+    photo_features = np.load(features_path / "features.npy")
+    photo_ids = pd.read_csv(features_path / "photo_ids.csv")
+    photo_ids = list(photo_ids["photo_id"])
+
+    # Load the open CLIP model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("checkpoints/ViT-B-32.pt", device=device)
+
     app.config["JSON_SORT_KEYS"] = False
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host="0.0.0.0", port=5001)
